f3eca64203050f33e18970d0ad9b7bd5a9dd79cf
yamith_1 <yamithmeneses122@gmail.com>
2026-08-20 09:00:26 -0500

    vigecimo intento

--- js/login.js
+++ js/login.js
@@ -34,13 +34,13 @@ boton.addEventListener("click", (evento) => {
     } else if (correo.value.trim() === "interprete@sena.com" && contraseña.value.trim() === "5678") {
 
         evento.preventDefault();
-        window.location.href = "../html/interprete.html";
+        window.location.href = "../html/Interprete.html";
 
     // LOGIN DEL ADMINISTRADOR
     } else if (correo.value.trim() === "admin@sena.com" && contraseña.value.trim() === "9012") {
 
         evento.preventDefault();
-        window.location.href = "../html/inicio_admin.html";
+        window.location.href = "../html/Inicio_admin.html";
 
     // CORREO O CONTRASEÑA INCORRECTOS
     } else {
