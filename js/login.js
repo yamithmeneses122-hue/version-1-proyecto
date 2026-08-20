0d4596bf2398d332334ad6d49c86218e042cab3b
yamith_1 <yamithmeneses122@gmail.com>
2026-08-19 21:05:56 -0500

    arreglos en angela de el boton

--- js/login.js
+++ js/login.js
@@ -4,18 +4,46 @@ const boton = document.querySelector(".btn-login");
 
 boton.addEventListener("click", (evento) => {
 
-    correo.value.trim() === "" &&
-    contraseña.value.trim() === "" &&
-    (evento.preventDefault(),
-    alert("Por favor, llena los campos de correo y contraseña"));
+    // VALIDAR SI LOS DOS CAMPOS ESTÁN VACÍOS
+    if (correo.value.trim() === "" && contraseña.value.trim() === "") {
 
-    correo.value.trim() === "" &&
-    contraseña.value.trim() !== "" &&
-    (evento.preventDefault(),
-    alert("Por favor, llena el campo de correo"));
+        evento.preventDefault();
+        alert("Por favor, llena los campos de correo y contraseña");
 
-    correo.value.trim() !== "" &&
-    contraseña.value.trim() === "" &&
-    (evento.preventDefault(),
-    alert("Por favor, llena el campo de contraseña"));
+    // VALIDAR SI EL CORREO ESTÁ VACÍO
+    } else if (correo.value.trim() === "" && contraseña.value.trim() !== "") {
+
+        evento.preventDefault();
+        alert("Por favor, llena el campo de correo");
+
+    // VALIDAR SI LA CONTRASEÑA ESTÁ VACÍA
+    } else if (correo.value.trim() !== "" && contraseña.value.trim() === "") {
+
+        evento.preventDefault();
+        alert("Por favor, llena el campo de contraseña");
+
+    // LOGIN DEL OPERADOR
+    } else if (correo.value.trim() === "operador@sena.com" && contraseña.value.trim() === "1234") {
+
+        evento.preventDefault();
+        window.location.href = "../html/inicio.html";
+
+    // LOGIN DEL INTÉRPRETE
+    } else if (correo.value.trim() === "interprete@sena.com" && contraseña.value.trim() === "5678") {
+
+        evento.preventDefault();
+        window.location.href = "../html/interprete.html";
+
+    // LOGIN DEL ADMINISTRADOR
+    } else if (correo.value.trim() === "admin@sena.com" && contraseña.value.trim() === "9012") {
+
+        evento.preventDefault();
+        window.location.href = "../html/administrador.html";
+
+    // CORREO O CONTRASEÑA INCORRECTOS
+    } else {
+
+        evento.preventDefault();
+        alert("Correo o contraseña incorrectos");
+    }
 });
